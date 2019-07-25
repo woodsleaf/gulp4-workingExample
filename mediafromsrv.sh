a829e0ff58037e31ea5c26448db88939b2f89337
woodsleaf <freepostpair@gmail.com>
2019-07-26 01:11:21 +0300

    first

---
+++ mediafromsrv.sh
@@ -0,0 +1,2 @@
+scp -rC -P 22 vickytorry@92.53.96.207:~/test/public_html/img/* ./public_html/img/
+scp -rC -P 22 vickytorry@92.53.96.207:~/test/public_html/media/* ./public_html/media/
---
+++ mediatosrv.sh
@@ -0,0 +1,2 @@
+scp -rC -P 22 ./public_html/img/* vickytorry@92.53.96.207:~/test/public_html/img/
+scp -rC -P 22 ./public_html/media/* vickytorry@92.53.96.207:~/test/public_html/media/
