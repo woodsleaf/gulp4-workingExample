1ccbe1d6b04e24dfbaf956a9e5c7c6987e30bdde
woodsleaf <freepostpair@gmail.com>
2019-08-03 17:31:50 +0300

    del scriptsmediacopybyssh

--- mediafromsrv.cmd
+++
@@ -1,2 +0,0 @@
-scp -rC -P 22 vickytorry@vh192.timeweb.ru:~/test/public_html/img/* ./public_html/img/
-scp -rC -P 22 vickytorry@vh192.timeweb.ru:~/test/public_html/media/* ./public_html/media/
--- mediatosrv.cmd
+++
@@ -1,2 +0,0 @@
-scp -rC -P 22 ./public_html/img/* vickytorry@vh192.timeweb.ru:~/test/public_html/img/
-scp -rC -P 22 ./public_html/media/* vickytorry@vh192.timeweb.ru:~/test/public_html/media/
